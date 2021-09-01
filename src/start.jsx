9e42f985ceaa434f546e157665f81d60455b14e1
HUESMario <happlem@yahoo.de>
2021-09-01 17:05:25 +0200

    Progress

--- src/start.jsx
+++ src/start.jsx
@@ -1,8 +1,41 @@
 import React from "react";
-import { Nav } from "./Nav";
+import "./start.scss";
 
-export const Start = () => {
+const Greeting = () => {
+    const hours = new Date().getHours();
+    let greeting;
+    let Name = "Mario";
+    if(hours < 6)
+        {
+            greeting = "Good Evening";
+        }
+        if(hours > 6)
+        {
+            greeting = "Good Morning";
+        }
+        if(hours > 12)
+        {
+            greeting = "Welcome";
+        }
+
+        if(hours > 18)
+        {
+            greeting = "Good Evening";
+        }
+return (
+<div id="Greetings">
+    <p>{greeting}, {Name}</p>
+    <h2>Let's get to Work.</h2>
+</div>
+)
+}
+
+export const Start = (props) => {
     return (
-        <Nav activeNav="start"/>
+    <div id="start">
+    <Greeting/>
+    <div id="tasks"></div>
+        
+    </div>
     )
 }
