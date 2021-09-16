7eef44b88be669b863364eedf56c950908792eff
HUESMario <happlem@yahoo.de>
2021-09-16 11:06:52 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,40 @@ class App extends Component
   constructor()
   {
     super();
-    this.state = {activePage: "start"};
+    this.state = {
+      activePage: "start",
+      To_Do: ""};
     this.changeNav = this.changeNav.bind(this);
+    this.addToDo =this.addToDo.bind(this);
   }
 
   changeNav = (changeSite) => {
     this.setState({
-        activePage: changeSite
+        activePage: changeSite,
+        To_Do: this.state.To_Do
     });
 }
+
+  addToDo = (todo) => {
+
+    let newToDo = this.state.To_Do;
+
+    if(todo)
+    {
+      
+    }
+
+    this.setState({
+      activePage:this.state.activePage,
+      To_Do: newToDo
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <Nav activeNav={this.state.activePage} changeSite={this.changeNav}/>
-        <Main activePage={this.state.activePage}/>
+        <Main activePage={this.state.activePage} todoList={this.state.To_Do} addToDo={this.addToDo}/>
       </div>
     );
   }
